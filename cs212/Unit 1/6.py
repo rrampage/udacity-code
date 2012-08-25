2be7be0405e55d9bae62c945a57a49f1663cf5fc
rrampage <raunak1001@gmail.com>
2012-08-25 13:37:58 +0530

    Answered Q6

--- cs212/Unit 1/6.py
+++ cs212/Unit 1/6.py
@@ -11,7 +11,7 @@
 
 def poker(hands):
     "Return the best hand: poker([hand,...]) => hand"
-    return max(hands)
+    return max(hands, key = hand_rank)
 
 def hand_rank(hand):
     return None # we will be changing this later.
